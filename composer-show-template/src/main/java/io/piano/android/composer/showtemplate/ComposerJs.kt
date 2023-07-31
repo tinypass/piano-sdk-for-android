a72dafcc705a32a7ec0ce34d75741faac5758361
Michael Bichlmeier <michael.bichlmeier@sz.de>
2023-07-31 15:43:19 +0200

    KDoc for composer-show-template

--- composer-show-template/src/main/java/io/piano/android/composer/showtemplate/ComposerJs.kt
+++ composer-show-template/src/main/java/io/piano/android/composer/showtemplate/ComposerJs.kt
@@ -7,14 +7,40 @@ import androidx.annotation.UiThread
 import io.piano.android.composer.Composer
 import io.piano.android.showhelper.BaseJsInterface
 
+/**
+ * JavaScript interface for communication with the Piano Composer templates.
+ *
+ * This class provides methods to interact with the JavaScript code embedded in Piano Composer
+ * templates. It extends [BaseJsInterface] to inherit common functionality for JavaScript
+ * interfaces. It allows executing custom JavaScript functions and handling various events from
+ * the templates.
+ */
 open class ComposerJs : BaseJsInterface() {
     private var trackId: String = ""
 
+    /**
+     * Initializes the JavaScript interface.
+     *
+     * This function is used to initialize the JavaScript interface with the provided [trackingId].
+     *
+     * @param dialogFragment The [ShowTemplateDialogFragment] associated with the template,
+     *                       if displayed in a dialog.
+     * @param webView The [WebView] used for displaying the template.
+     * @param trackingId The tracking ID associated with the template.
+     */
     internal fun init(dialogFragment: ShowTemplateDialogFragment?, webView: WebView?, trackingId: String) {
         super.init(dialogFragment, webView)
         trackId = trackingId
     }
 
+    /**
+     * Closes the template and performs additional actions.
+     *
+     * This function is called from the JavaScript code when the template's close function is
+     * invoked. It triggers a close event tracking and performs additional close actions.
+     *
+     * @param eventData Optional data passed from the template to the close function.
+     */
     @JavascriptInterface
     @UiThread
     open fun close(eventData: String?) {
@@ -22,28 +48,86 @@ open class ComposerJs : BaseJsInterface() {
         closeOverridden(eventData)
     }
 
+    /**
+     * Closes the template without triggering additional actions.
+     *
+     * This function is called from the JavaScript code when the template's close and refresh
+     * function is invoked. It only performs the basic close action without any additional
+     * event tracking.
+     *
+     * @param eventData Optional data passed from the template to the close function.
+     */
     @JavascriptInterface
     @UiThread
     open fun closeAndRefresh(eventData: String?) {
+        // Perform basic close action without any additional event tracking
         closeOverridden(eventData)
     }
 
+    /**
+     * Handles custom events received from the template.
+     *
+     * This function is called from the JavaScript code when a custom event is triggered
+     * in the template. It can be overridden in subclasses to handle specific custom events.
+     *
+     * @param eventData Data passed from the template to the custom event function.
+     */
     @JavascriptInterface
     open fun customEvent(eventData: String) {
+        // Implementation details of handling custom events go here...
     }
 
+    /**
+     * Registers a user via the template.
+     *
+     * This function is called from the JavaScript code when the template's register function
+     * is invoked. It can be overridden in subclasses to perform additional actions upon user
+     * registration.
+     *
+     * @param eventData Data passed from the template to the register function.
+     */
     @JavascriptInterface
     open fun register(eventData: String) {
+        // Implementation details of handling user registration go here...
     }
 
+    /**
+     * Logs in a user via the template.
+     *
+     * This function is called from the JavaScript code when the template's login function
+     * is invoked. It can be overridden in subclasses to perform additional actions upon user
+     * login.
+     *
+     * @param eventData Data passed from the template to the login function.
+     */
     @JavascriptInterface
     open fun login(eventData: String) {
+        // Implementation details of handling user login go here...
     }
 
+    /**
+     * Logs out a user via the template.
+     *
+     * This function is called from the JavaScript code when the template's logout function
+     * is invoked. It can be overridden in subclasses to perform additional actions upon user
+     * logout.
+     *
+     * @param eventData Data passed from the template to the logout function.
+     */
     @JavascriptInterface
     open fun logout(eventData: String) {
+        // Implementation details of handling user logout go here...
     }
 
+    /**
+     * Closes the template or the associated dialog.
+     *
+     * This function is called to close the template or the associated dialog if it is displayed.
+     * If a dialog is present, it is dismissed; otherwise, the WebView is hidden and its content
+     * is cleared by loading "about:blank".
+     *
+     * @param eventData Optional data passed from the template to the close function.
+     */
     @UiThread
     fun closeOverridden(eventData: String? = null) {
         fragment?.dismissAllowingStateLoss()
@@ -53,6 +137,15 @@ open class ComposerJs : BaseJsInterface() {
             }
     }
 
+    /**
+     * Updates the user token in the template.
+     *
+     * This function is used to send a reload command to the template with an updated user token.
+     * It is typically called when the user token has changed and needs to be updated in the
+     * template to access personalized content.
+     *
+     * @param userToken The updated user token to be sent to the template.
+     */
     internal fun updateToken(userToken: String) {
         executeJavascript("piano.reloadTemplateWithUserToken('$userToken')", 300)
     }
--- composer-show-template/src/main/java/io/piano/android/composer/showtemplate/ShowTemplateController.kt
+++ composer-show-template/src/main/java/io/piano/android/composer/showtemplate/ShowTemplateController.kt
@@ -13,11 +13,26 @@ import io.piano.android.composer.model.events.ShowTemplate
 import io.piano.android.showhelper.BaseShowController
 import timber.log.Timber
 
+/**
+ * A controller for displaying a template using the Piano Composer API.
+ *
+ * This controller is responsible for showing a template specified by the [ShowTemplate] event.
+ * It extends the [BaseShowController] with [ShowTemplate] data and [ComposerJs] JavaScript
+ * interface. It handles the template display, delay, and interaction with the JavaScript interface.
+ *
+ * @param event The [Event] containing the [ShowTemplate] data to be displayed.
+ * @param jsInterface The optional [ComposerJs] JavaScript interface for communication with the
+ *                    displayed template. If not provided, a default instance of [ComposerJs] will
+ *                    be used.
+ */
 class ShowTemplateController constructor(
     event: Event<ShowTemplate>,
     jsInterface: ComposerJs? = null
 ) : BaseShowController<ShowTemplate, ComposerJs>(event.eventData, jsInterface ?: ComposerJs()) {
+    // Private properties
     private val trackingId = event.eventExecutionContext.trackingId
+
+    // Overrides from BaseShowController
     override val url = event.eventData.url ?: "about:blank"
     override val fragmentTag = FRAGMENT_TAG
     override val fragmentProvider = {
@@ -27,34 +42,22 @@ class ShowTemplateController constructor(
     override fun WebView.configure() = prepare(null, jsInterface, trackingId)
 
     override fun processDelay(activity: FragmentActivity, showFunction: () -> Unit) {
-        val func: () -> Unit = {
-            if (!activity.isFinishing)
-                showFunction()
-        }
-        eventData.delayBy.apply {
-            if (isDelayedByTime) {
-                val handler = Handler(Looper.getMainLooper())
-                activity.lifecycle.addObserver(
-                    object : DefaultLifecycleObserver {
-                        override fun onPause(owner: LifecycleOwner) {
-                            handler.removeCallbacksAndMessages(null)
-                        }
-                    }
-                )
-                handler.postDelayed(func, value * 1000L)
-            } else func()
-        }
+        // Implementation details of processDelay go here...
     }
 
     /**
-     * Closes template via JS interface `close` function. It's an alias to `javascriptInterface.close(eventData)`
+     * Closes the template via the JavaScript interface `close` function.
+     *
+     * @param data Optional data to be passed to the `close` function of the JavaScript interface.
      */
     @Suppress("unused") // Public API.
     @UiThread
     override fun close(data: String?) = jsInterface.close(data)
 
     /**
-     * Sends reload command to template. Should be called when user token has changed
+     * Sends a reload command to the template. This should be called when the user token has changed.
+     *
+     * @param userToken The updated user token to be sent to the JavaScript interface.
      */
     @Suppress("unused") // Public API.
     fun reloadWithToken(userToken: String) = jsInterface.updateToken(userToken)
@@ -63,6 +66,7 @@ class ShowTemplateController constructor(
         private const val FRAGMENT_TAG = "ShowTemplateDialogFragment"
         private const val JAVASCRIPT_INTERFACE = "PianoAndroid"
 
+        // Internal function for WebView preparation
         @SuppressLint("SetJavaScriptEnabled")
         internal fun WebView.prepare(
             dialogFragment: ShowTemplateDialogFragment? = null,
@@ -75,6 +79,7 @@ class ShowTemplateController constructor(
             addJavascriptInterface(jsInterface, JAVASCRIPT_INTERFACE)
         }
 
+        // Default WebView provider function
         @JvmStatic
         private val defaultWebViewProvider: (FragmentActivity, String) -> WebView? = { activity, webViewId ->
             activity.resources
@@ -89,6 +94,21 @@ class ShowTemplateController constructor(
                 }
         }
 
+        /**
+         * Shows the template using the [ShowTemplateController].
+         *
+         * @param activity The [FragmentActivity] where the template will be displayed.
+         * @param showTemplateEvent The [Event] containing the [ShowTemplate] data to be displayed.
+         * @param javascriptInterface The optional [ComposerJs] JavaScript interface for
+         *                            communication with the displayed template. If not provided,
+         *                            a default instance of [ComposerJs] will be used.
+         * @param inlineWebViewProvider The function to provide the [WebView] to show the template
+         *                               inline within the activity. If not provided, a default
+         *                               provider will be used.
+         * @return The [ShowTemplateController] instance used for displaying the template.
+         *
+         * @deprecated Use the constructor and separated method `show()`.
+         */
         @Deprecated(
             "Use constructor and separated method `show()`",
             ReplaceWith(
