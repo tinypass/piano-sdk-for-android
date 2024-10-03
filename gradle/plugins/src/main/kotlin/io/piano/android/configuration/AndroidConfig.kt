463c8563ad48953c7bb7dc5983689bde6b5687c5
Dmitriy <dekanszn@gmail.com>
2024-10-04 00:36:15 +0400

    Update dependencies

--- gradle/plugins/src/main/kotlin/io/piano/android/configuration/AndroidConfig.kt
+++ gradle/plugins/src/main/kotlin/io/piano/android/configuration/AndroidConfig.kt
@@ -3,10 +3,10 @@ package io.piano.android.configuration
 import org.gradle.api.JavaVersion
 
 internal object AndroidConfig {
-    const val androidBuildTools = "34.0.0"
-    const val androidMinSdk = 21
-    const val androidTargetSdk = 34
-    const val androidCompileSdk = 34
+    const val androidBuildTools = "35.0.0"
+    const val androidMinSdk = 23
+    const val androidTargetSdk = 35
+    const val androidCompileSdk = 35
     val compileSourceVersion = JavaVersion.VERSION_1_8
     val compileTargetVersion = JavaVersion.VERSION_1_8
 }
